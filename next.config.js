464602677da9f105f8a3dde9908c8a1534509972
agenda23 <agenda23@gmail.com>
2025-06-15 20:03:06 +0900

    feat: PWAのフルスクリーン表示対応

--- next.config.js
+++ next.config.js
@@ -4,12 +4,23 @@ const withPWA = nextPWA({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development'
+  disable: process.env.NODE_ENV === 'development',
+  buildExcludes: [/middleware-manifest\.json$/],
+  cacheOnFrontEndNav: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  fallbacks: {
+    document: '/offline',
+  }
 })
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ['ja'],
+    defaultLocale: 'ja',
+  }
 }
 
 export default withPWA(nextConfig) 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,14 +4,18 @@ export default function Document() {
   return (
     <Html lang="ja">
       <Head>
+        <title>トレーニングタイマー</title>
         <meta name="application-name" content="トレーニングタイマー" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="トレーニングタイマー" />
         <meta name="description" content="シンプルで使いやすいインターバルトレーニングタイマー" />
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-TileColor" content="#ef4444" />
+        <meta name="msapplication-tap-highlight" content="no" />
         <meta name="theme-color" content="#ef4444" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover, user-scalable=no" />
 
         <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
