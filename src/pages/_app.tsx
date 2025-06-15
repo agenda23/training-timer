7aacd1de1285f8e2454e92df373fb87d60d800eb
agenda23 <agenda23@gmail.com>
2025-06-15 20:20:12 +0900

    fix: interフォントの未使用エラーを修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,21 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { Inter } from 'next/font/google'
 import Head from 'next/head'
+import { Inter } from 'next/font/google'
 import { Navigation } from '@/components/Navigation'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <div className={inter.className}>
       <Head>
         <title>トレーニングタイマー</title>
       </Head>
-      <div className="min-h-screen pb-16">
+      <div className="min-h-screen pb-16 touch-none">
         <Component {...pageProps} />
       </div>
       <Navigation />
-    </>
+    </div>
   )
 } 
